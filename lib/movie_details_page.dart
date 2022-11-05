b395b667b288b1beb26d57b76ac19bd929ba251d
Diana Stoica <diana-c.stoica@outlook.com>
2022-11-05 21:18:16 +0200

    Initial setup

---
+++ lib/movie_details_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MovieDetailsPage extends StatelessWidget {
+  const MovieDetailsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

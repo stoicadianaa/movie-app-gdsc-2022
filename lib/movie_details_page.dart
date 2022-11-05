f9e36f02dfd7e9c999d6e1d82e599353a89cf8b8
Diana Stoica <diana-c.stoica@outlook.com>
2022-11-05 21:30:58 +0200

    Small changes to the MovieDetails page

--- lib/movie_details_page.dart
+++ lib/movie_details_page.dart
@@ -5,6 +5,11 @@ class MovieDetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    
+    return Scaffold(
+      body: Container(),
+    );
   }
 }
